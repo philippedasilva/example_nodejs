8b7cc58a505c0b7947e276ab34d80c38ff57e6a6
Phil <philippe.dasilva@gmx.fr>
2015-11-10 11:53:55 +0100

    Notre premier commit :)

---
+++ src/server.js
@@ -0,0 +1,16 @@
+var http=require('http');
+var user=require('./user.js');
+
+http.createServer(function(req,res){
+	
+	res.writeHead(200,{content:'application/json'});
+	user.get(11,function (user){
+		var response={
+		info:"here's your user !!",
+		user:user
+	}
+	res.end(JSON.stringify(response));
+	})
+	
+	
+}).listen(1337,'127.0.0.1');
---
+++ src/user.js
@@ -0,0 +1,20 @@
+module.exports={
+	save:function(name,callback){
+		//My code
+		var user={
+			id:"2222",
+			name:name
+		}
+		callback(user);
+	}
+	get:function(id,callback){
+		//get a user
+		
+		var user={
+			name:"cesar",
+			id:id
+		}
+		callback(user);
+	}
+
+}
