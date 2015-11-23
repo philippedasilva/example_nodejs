b5b0e21841a6de3c7f8616fbea5a1f2469ded0d0
Phil <philippe.dasilva@gmx.fr>
2015-11-24 00:46:52 +0100

    Implementation des 2 scripts (start et test)

--- src/user.js
+++ src/user.js
@@ -1,14 +1,13 @@
 module.exports={
 	save:function(name,callback){
-		//My code
+		
 		var user={
 			id:"2222",
 			name:name
 		}
 		callback(user);
-	}
+	},
 	get:function(id,callback){
-		//get a user
 		
 		var user={
 			name:"phil",
